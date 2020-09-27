1bbc91a6dd68f150e2075b4e4f8edb717e7f7804
zwdnet <zwdnet@163.com>
2020-09-27 21:45:51 +0800

    更新，到服务器上运行。

--- testPytorch.py
+++ testPytorch.py
@@ -68,7 +68,36 @@ def tensor_nn():
         w2 -= learning_rate * grad_w2
         
         
+# 自动梯度
+def AG():
+    import torch
+    
+    dtype = torch.float
+    device = torch.device("cpu")
+    N, D_in, H, D_out = 64, 1000, 100, 10
+    
+    x = torch.randn(N, D_in, device=device, dtype=dtype)
+    y = torch.randn(N, D_out, device=device, dtype=dtype)
+    
+    w1 = torch.randn(D_in, H, device=device, dtype=dtype, requires_grad=True)
+    w2 = torch.randn(H, D_out, device=device, dtype=dtype, requires_grad=True)        
+           
+    learning_rate = 1e-6
+    for t in range(500):
+        y_pred = x.mm(w1).clamp(min=0).mm(w2)
+        loss = (y_pred - y).pow(2).sum()
+        if t % 100 == 99:
+            print(t, loss)
+        loss.backward()
+        with torch.no_grad():
+            w1 -= learning_rate * grad_w1
+            w2 -= learning_rate * grad_w2
+            w1.grad.zero_()
+            w2.grad.zero_()
+        
+        
 if __name__ == "__main__":
     WU()
     tensor_nn()
+    AG()
     
