1f0ded5f67d34785cac82902f84bb4e7953d6288
zwdnet <zwdnet@163.com>
2020-09-28 09:52:05 +0800

    尝试使用pytorch的nn模块。

--- testPytorch.py
+++ testPytorch.py
@@ -140,9 +140,42 @@ def DefFB():
             w1.grad.zero_()
             w2.grad.zero_()
             
+            
+# 用nn模块定义神经网络
+def nn_test():
+    import torch
+    
+    N, D_in, H, D_out = 64, 1000, 100, 10
+    x = torch.randn(N, D_in)
+    y = torch.randn(N, D_out)
+    
+    model = torch.nn.Sequential(
+        torch.nn.Linear(D_in, H),
+        torch.nn.ReLU(),
+        torch.nn.Linear(H, D_out),
+    )
+    
+    loss_fn = torch.nn.MSELoss(reduction = "sum")
+    
+    learning_rate = 1e-4
+    for t in range(500):
+        y_pred = model(x)
+        loss = loss_fn(y_pred, y)
+        if t % 100 == 99:
+            print(t, loss)
+            
+        model.zero_grad()
+        loss.backward()
+        
+        with torch.no_grad():
+            for param in model.parameters():
+                param -= learning_rate*param.grad
+    
+            
         
 if __name__ == "__main__":
     WU()
     tensor_nn()
     AG()
     DefFB()
+    nn_test()
